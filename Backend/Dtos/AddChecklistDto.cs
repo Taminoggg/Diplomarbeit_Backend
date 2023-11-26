44a14735d6224ced68419e172b2d5453709dd49a
Taminogg <91053889+Taminogg@users.noreply.github.com>
2023-11-26 11:08:01 +0100

    Steps reworked

--- Backend/Dtos/AddChecklistDto.cs
+++ Backend/Dtos/AddChecklistDto.cs
@@ -3,4 +3,6 @@ namespace ContainerToolDBDb;
 public class AddChecklistDto
 {
     [Required] public string CustomerName { get; set; } = null!;
+    [Required] public int OrderId { get; set; }
+    [Required] public List<int> StepIds { get; set; } = null!;
 }
--- Backend/Dtos/ChecklistDto.cs
+++ Backend/Dtos/ChecklistDto.cs
@@ -4,4 +4,5 @@ public class ChecklistDto
 {
     [Required] public int Id { get; set; }
     [Required] public string CustomerName { get; set; } = null!;
+    [Required] public List<Step> Steps { get; set; } = null!;
 }
